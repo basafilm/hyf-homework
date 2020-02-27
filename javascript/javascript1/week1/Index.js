a872c111a4d411b4b4f81bb91294bc442beec93b
basafilm <basafilm@gmail.com>
2020-02-27 23:18:04 +0100

    The house priecy corrected

--- javascript/javascript1/week1/Index.js
+++ javascript/javascript1/week1/Index.js
@@ -20,28 +20,22 @@ console.log('My age in:' +' ' +yearFuture +' ' +'will be:' +age +' ' + 'year old
  
  var houseyPricey;
  //Housey pricey (A house price estimator)
- volumeInMeters=[['peter', 8 , 10, 10],['julie', 5, 11, 8]];
+ volumeInMeters=[['peter', 8 * 10* 10],['julie', 5* 11* 8]];
  gardenSizeInM2 =[[100],[70]];
  peterBuy =true;
  julieBuy= true;
  if(peterBuy){
-     /*
-     volumeInMeters=[8 , 10, 10];
-     gardenSizeInM2 =100;
-     */
+
      houseCost =25000000;
      housePrice = volumeInMeters[0][1] * 2.5 * 1000 + gardenSizeInM2[0] *300;
      console.log('Peter\'s house price should be:' +' ' +housePrice +' ' +'It is'+' ' +[houseCost -housePrice] +' ' +'higher than the market prices.');
  }
  
  else(julieBuy)
- /*
-     volumeInMeters=[5 , 11, 8];
-     gardenSizeInM2 =70;
-     */
+
      houseCost =1000000;
      housePrice = volumeInMeters[1][1] * 2.5 * 1000 + gardenSizeInM2[1][0] *300;
-     console.log('Julie\'s house price should be:' +' ' +housePrice +' ' +'It is'+' ' +[houseCost -housePrice] +' ' +'higher than the market prices.');
+     console.log('Julie\'s house price should be:' +' ' +housePrice +' ' +'It is'+' ' +[houseCost -housePrice] +' ' +'lower than the market prices.');
   
       //Ez Namey (Startup name generator)
  
