3d36703a16c2db83a6d1b9bb22897b44dd535dae
basafilm <basafilm@gmail.com>
2020-02-27 21:29:40 +0100

    The dog age calculator added

--- javascript/javascript1/week1/Index.js
+++ javascript/javascript1/week1/Index.js
@@ -3,3 +3,18 @@ let yearOfBirth = 1974;
 let yearFuture = 2050;
 age = yearFuture - yearOfBirth;
 console.log('My age in:' +' ' +yearFuture +' ' +'will be:' +age +' ' + 'year old.');
+
+ // Goodboy-Oldboy (A dog age calculator)
+ const dogYearOfBirth =2017;
+ const dogYearFuture =2027;
+ let dogYear = 7;
+ let shouldShowResultInDogYears = true;
+ if(shouldShowResultInDogYears) {
+     dogAge = dogYearFuture -dogYearOfBirth;
+     console.log('Your dog will be' +' ' +dogAge *dogYear +' ' +'year old in' +' ' +dogYearFuture +'.');
+ }
+ 
+ else 
+ dogAge = dogYearFuture -dogYearOfBirth;
+ console.log('Your dog will be' +' ' +dogAge +' ' +'human years old in' +' ' +dogYearFuture +'.');
+ 
