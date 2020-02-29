5d14621063b04f5cf4e004fa18da796d69c3452a
basafilm <basafilm@gmail.com>
2020-02-29 14:38:07 +0100

    Corrected based on the suggestions

--- javascript/javascript1/week1/Index.js
+++ javascript/javascript1/week1/Index.js
@@ -1,46 +1,45 @@
 //Age-ify (A future age calculator)
-let yearOfBirth = 1974;
-let yearFuture = 2050;
-age = yearFuture - yearOfBirth;
+const yearOfBirth = 1974;
+const yearFuture = 2050;
+let age = yearFuture - yearOfBirth;
 console.log('My age in:' +' ' +yearFuture +' ' +'will be:' +age +' ' + 'year old.');
 
  // Goodboy-Oldboy (A dog age calculator)
  const dogYearOfBirth =2017;
  const dogYearFuture =2027;
- let dogYear = 7;
- let shouldShowResultInDogYears = true;
+ const dogYear = 7;
+ const shouldShowResultInDogYears = true;
+ let dogAge = dogYearFuture -dogYearOfBirth;
+
  if(shouldShowResultInDogYears) {
-     dogAge = dogYearFuture -dogYearOfBirth;
      console.log('Your dog will be' +' ' +dogAge *dogYear +' ' +'year old in' +' ' +dogYearFuture +'.');
  }
- 
  else 
- dogAge = dogYearFuture -dogYearOfBirth;
  console.log('Your dog will be' +' ' +dogAge +' ' +'human years old in' +' ' +dogYearFuture +'.');
  
  var houseyPricey;
  //Housey pricey (A house price estimator)
- volumeInMeters=[['peter', 8 * 10* 10],['julie', 5* 11* 8]];
- gardenSizeInM2 =[[100],[70]];
- peterBuy =true;
- julieBuy= true;
+ const volumeInMeters=[['peter', 8 * 10* 10],['julie', 5* 11* 8]];
+ const gardenSizeInM2 =[[100],[70]];
+ const peterHouseCost =25000000;
+ const julieHouseCost =1000000;
+ const marketHousePrice1 = volumeInMeters[0][1] * 2.5 * 1000 + gardenSizeInM2[0] *300;
+ const marketHousePrice2 = volumeInMeters[1][1] * 2.5 * 1000 + gardenSizeInM2[1][0] *300;
+ const peterBuy =true;
+ const julieBuy= true;
  if(peterBuy){
-
-     houseCost =25000000;
-     housePrice = volumeInMeters[0][1] * 2.5 * 1000 + gardenSizeInM2[0] *300;
-     console.log('Peter\'s house price should be:' +' ' +housePrice +' ' +'It is'+' ' +[houseCost -housePrice] +' ' +'higher than the market prices.');
+     console.log('Peter\'s house based on market price should be:' +' ' +marketHousePrice1 +' ' +'It is'+' ' +[peterHouseCost -marketHousePrice1] +' ' +'higher than the market prices.');
  }
  
  else(julieBuy)
-
-     houseCost =1000000;
-     housePrice = volumeInMeters[1][1] * 2.5 * 1000 + gardenSizeInM2[1][0] *300;
-     console.log('Julie\'s house price should be:' +' ' +housePrice +' ' +'It is'+' ' +[houseCost -housePrice] +' ' +'lower than the market prices.');
+     console.log('Julie\'s house based on market price is:' +' ' +marketHousePrice2 +' ' +'It is'+' ' +[julieHouseCost -marketHousePrice2] +' ' +'lower than that, reasonable to buy.');
   
-      //Ez Namey (Startup name generator)
- 
+ //Ez Namey (Startup name generator)
  const firstWords =["Easy", "Awesome", "Corporate","Creative","Intelegent","Smart","Fast","Accessable","Honest","Professional", "First Hand"];
  const secondWords =["Operator", "Agent", "Brothers","Fifty,Fifty","Elegant","Venlig","Org","Fragment","Power","Perfect", "Lasting"];
- let randomNumber = Math.floor(Math.random() * 10) + 0;
- let startupName = firstWords[randomNumber] +" " + secondWords[randomNumber];
+ const randomNumber = Math.floor(Math.random() * 10) + 0;
+ const random = Math.floor(Math.random() * 10) + 0;
+ let startupName = firstWords[randomNumber] +" " + secondWords[random];
  console.log( "Print " + startupName + " " +startupName.length +" " +"Characters");
+
+ 
