04fbe9bc6758b263e62c83c77574937bfc7a5f6f
Malek Shafi'i <malekshafii@Maleks-iMac.local>
2020-03-05 23:05:08 +0100

    The UseFormalName added to getFullName

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -1,6 +1,20 @@
 
-function getFullName(firstName, surName){
+/*function getFullName(firstName, surName){
     return firstName + surName;
 }
 const fullname1= getFullName('Malek ', 'Shafii');
 console.log(fullname1)
+*/
+
+
+function getFullName(firstName, surName, useFormalName){
+
+}
+const firstName= 'Elyas';
+const surName= 'Yuresh';
+let useFormalName= true;  
+if(useFormalName==true){
+    console.log('Lord!' +' ' + firstName +' ' +surName)
+}else{
+    console.log(firstName+' '+surName)
+}
