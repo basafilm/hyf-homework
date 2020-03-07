6c1a92fbee8c4939580e5850c8903d0db15e30e2
Malek Shafi'i <malekshafii@Maleks-iMac.local>
2020-03-07 23:41:01 +0100

    weekDayEvent and wearSomething functions added

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -10,11 +10,37 @@ console.log(fullname1)
 function getFullName(firstName, surName, useFormalName){
 
 }
-const firstName= 'Elyas';
-const surName= 'Yuresh';
+const firstName= '';
+const surName= '';
 let useFormalName= true;  
 if(useFormalName==true){
     console.log('Lord!' +' ' + firstName +' ' +surName)
 }else{
     console.log(firstName+' '+surName)
 }
+
+
+
+function weekdayEvent() {
+let weekDays= ['Sunday','Monday','Tursday','Wedensday','Thursday', 'Friday', 'Sutarday'];
+let today= new Date();
+let day = today.getDay();
+let eventDay = day  % 7;
+return weekDays[eventDay];
+
+}
+console.log(weekdayEvent(9));
+
+
+function wearSomeThing(temprature){
+    
+    if(temprature>=18){
+        console.log(" Wear T-Shert")
+    }else if(temprature< 18 && temprature>  15 ){
+        console.log( "Wear somethings warm!")
+    } else{
+        console.log("Wear Something Really Warm!")
+    }
+}
+
+wearSomeThing(10);
