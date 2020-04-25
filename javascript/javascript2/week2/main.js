ff5a1cf731602e3615d8c435ccf246b85a726814
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-04-25 17:10:52 +0200

    some chnages made to week2 Js2

--- javascript/javascript2/week2/main.js
+++ javascript/javascript2/week2/main.js
@@ -1,22 +1,29 @@
-//const testProductNames = ['Flat screen', 'Mobile phone', 'Wallet'];
-const products = getAvailableProducts()
-console.log(products);
 
-function renderProducts(products){
-    const ulFirstSet = document.querySelector('section.products > ul');
-    products.forEach(element => {
-        const liList = document.createElement('li');
-        const countries= element.shipsTo.reduce((accumulator , currentvalue)=> `<ul><li>${accumulator}</li><li>${currentvalue}</li></ul>`);
-        liList.innerHTML = `
-        <ul>
-          <li> |${element.name}| </li>
-          <li>|${element.price}|</li>
-          <li>|${element.rating}|</li>
-          <li>|${countries}|</li>
-        </ul>`;
-        ulFirstSet.appendChild(liList);
-    });        
-};
+console.log('Script loaded');
+
+const products = getAvailableProducts();
+const productsUl = document.querySelector('section.products ul');
+console.log(productsUl);
+
+function renderProducts(products) {
+    products.forEach(product => {
+        const li = document.createElement('li');
+
+        let shipsToHTML = '';
+        product.shipsTo.forEach(country => shipsToHTML += `<li>${country}</li>`);
+
+        li.innerHTML = `
+            <ul>
+                <li>${product.name}</li>
+                <li>${product.price}</li>
+                <li>${product.rating}</li>
+                <ul class="ships-to">${shipsToHTML}</ul>
+            </ul>
+        `;
+        productsUl.appendChild(li);
+    });
+}
+
 renderProducts(products);
 
 
@@ -28,13 +35,52 @@ const doubleOdNumber= numbers.filter((value)=>{
 console.log(doubleOdNumber);
 
 
+
 const searchBar = document.querySelector('div.search > input');
-const productNames = document.querySelector('div.products >ul'); 
+const productNames = document.querySelector('section.products ul '); 
+//console.log(productNames)
 
 searchBar.addEventListener('keyup', filterproducts);
 function filterproducts(){
     productNames.textContent = '';
-    const filteredData = products.filter(element => element.name.toLowerCase().includes(searchBar.value));
+    const filteredData = products.filter(product => product.name.toLowerCase().includes(searchBar.value));
     renderProducts(filteredData);
 };
 renderProducts(products);
+
+const sortItems = document.querySelector('.sort > select');
+console.log(sortItems);
+sortItems.addEventListener('change',sortProducts)
+function sortProducts(){
+    const items =sortItems.value; 
+    // console.log(items)
+    //const items = sortItems.options[sortItems.selectedIndex].value;
+    for ( let i=0; i<items.length; i++ ){
+        // const item =items[i];
+        // console.log(item)
+    if( items==='cheap'){
+      const cheapItems = products.sort((a, b)=> a.price - b.price);
+      //console.log(cheapItems)
+      renderProducts(cheapItems);
+    } if(items==='expensive') {
+        const expensiveItems = products.sort((a, b)=>b.price - a.price);
+        renderProducts(expensiveItems);
+    }
+    }
+};
+sortProducts(products);
+
+
+// sortItems.addEventListener('change', () =>{
+//     const items = sortItems.options[sortItems.selectedIndex].value;
+//     console.log(items)
+//     if(items==='cheap'){
+//         const cheapItems = products.sort((a, b)=> a.price - b.price);
+//         renderProducts(cheapItems); 
+//     }
+// if (items==='expensive'){
+//     const expensiveItems = products.sort((a, b)=>a.price - a.price);
+//     renderProducts(expensiveItems);
+// }
+// return products;
+// })
