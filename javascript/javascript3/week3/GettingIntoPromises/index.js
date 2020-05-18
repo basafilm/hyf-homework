bf40bd73827f497f94355d831b4c0cd9a247889d
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-05-18 20:40:08 +0200

    chnages on to get to promise

--- javascript/javascript3/week3/GettingIntoPromises/index.js
+++ javascript/javascript3/week3/GettingIntoPromises/index.js
@@ -1,37 +1,34 @@
 //Getting into promises
 
   let names =['Aswini-D3', 'tariqjavid', 'LucyChyzhova' ]
-  let repositories = names.map(name=> fetch(`https://api.github.com/search/repositories?q=user:${name}`));
-  const prom= Promise.all(repositories)
-   setTimeout(async function() {
-       return prom
-       .then(response =>{
-        const swiniRepo = response[0].json()
-        const tariqRepo = response[1].json()
-        const lucyRepo = response[2].json()
-        return Promise.all([swiniRepo,tariqRepo,lucyRepo ])
-    })
-    //render the fullname of the repo, url of the repo, and the owner
-    .then((result) =>{
-      console.log( result)
-      const arryOfObjects = result.map(re=> re.items);
-            arryOfObjects.forEach(item =>{
-             const repoNames = `<li> Full Name Of the Repo :  ${item[0].full_name}</li>`;
-             const urlRepos = `<li> Url of the Repo :  ${item[0].html_url}</li>`
-             const ownerOftheRepo = `<li> Owner of the Repo :  ${item[0].owner.login}</li>`
-         
- 
-         const outPut = document.querySelector('div')
-         const lists =document.createElement('ul');
-         outPut.appendChild(lists)
-         lists.innerHTML =`
-          ${repoNames}
-         ${urlRepos}
-          ${ownerOftheRepo}
-         
-         `
-     }) 
-    })
-   }, 2000)
-   
+  let name = names.forEach(user => user);
+  let repoUrls = `https://api.github.com/search/repositories?q=user:`;
+   const getUrl = async (user) =>{
+       const response = await fetch(repoUrls.concat(user))
+        const result = await response.json()
+        //console.log(result)
+        return result
+    }
+    console.log(getUrl('Aswini-D3'))
+
+  const outPut = async (user1, user2, user3) => {
+    const allPromisies = [getUrl(user1),getUrl(user2),getUrl(user3)]
+    const getUsers = await Promise.all(allPromisies)
+    const users = getUsers.map( result =>result.items)
+    const getBody = document.querySelector('body');
+    users.map((res) => {
+         const h5Tage = document.createElement('h5')
+         h5Tage.innerHTML = `Repo's Owner Name : ${res[0].owner.login}`
+         getBody.appendChild(h5Tage);
+        res.forEach( item => {
+          const ulList = document.createElement('ul');
+          const liList = document.createElement('li')
+          liList.innerHTML =item.name
+          ulList.appendChild(liList);
+          getBody.appendChild(ulList );
+        })
+       })
+  }
+  outPut('Aswini-D3', 'tariqjavid', 'LucyChyzhova');
+
 
