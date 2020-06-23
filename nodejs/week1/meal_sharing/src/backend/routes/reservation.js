aaab72c192ca1772149a6c4dad7dbb0e78b61ccd
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-06-23 04:01:24 +0200

    update on homework

--- nodejs/week1/meal_sharing/src/backend/routes/reservation.js
+++
@@ -1,8 +0,0 @@
-const resarvations = require('../data/reservations.json');
-
-// geting a single reservation by random 
-module.exports = function randomSingleReservation(req, res){
-      res.send(
-          resarvations[Math.floor(Math.random() * resarvations.length)]
-              )
-   };
